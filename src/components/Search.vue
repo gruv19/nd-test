62752f6cd635e3a4d1764fc0f4d9bbcf7f6fe099
gruv19 <grv1992@yandex.ru>
2022-10-20 21:40:50 +0500

    feat: Добавил базовую разметку компонента Search

---
+++ src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="search">
+    <form action="#" class="search__form">
+      <input type="text" class="search__input">
+      <button type="submit" class="search__submit">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="13.5" cy="10.5" r="5.5" stroke="#0066B3" stroke-width="2"/>
+          <line x1="9.70711" y1="15.7071" x2="4.70711" y2="20.7071" stroke="#0066B3" stroke-width="2"/>
+        </svg>
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search", // eslint-disable-line
+}
+</script>
