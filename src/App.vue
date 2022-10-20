8117b578f1e8b943f6f2f383f2895d90bcead1ae
gruv19 <grv1992@yandex.ru>
2022-10-20 21:46:17 +0500

    feat: Добавил базовую разметку главного компонента

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <div></div>
+  <main class="main">
+    <div class="main__header">
+      <Header />
+    </div>
+    <div class="main__search">
+      <Search />
+    </div>
+    <div class="main__manage">
+      <div class="main__manage-count">100 результатов</div>
+      <Sort />
+    </div>
+    <div class="main__cards">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    <div class="main__pagination"></div>
+  </main>
 </template>
 
 <script>
 
+import Header from "@/components/Header.vue";
+import Search from "@/components/Search.vue";
+import Sort from "@/components/Sort.vue";
+import Card from "@/components/Card.vue";
+
 export default {
   name: 'App',
+  components: { Header, Search, Sort, Card },
 }
 </script>
