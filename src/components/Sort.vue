6fff3782627ba8f745c606db765c1cedbb145cc9
gruv19 <grv1992@yandex.ru>
2022-10-20 21:40:19 +0500

    feat: Добавил базовую разметку компонента Sort

---
+++ src/components/Sort.vue
@@ -0,0 +1,14 @@
+<template>
+  <a href="#" class="search">
+    <span class="search__text">Цене</span>
+    <svg class="search__icon" width="7" height="8" viewBox="0 0 7 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M3.5 0V7M3.5 7L1 4.5M3.5 7L6 4.5" stroke="#EB3737"/>
+    </svg>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "Sort", // eslint-disable-line
+}
+</script>
