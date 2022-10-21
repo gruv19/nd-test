48bef4f4141a4bb99e15d65182dc04299ed8d582
gruv19 <grv1992@yandex.ru>
2022-10-21 19:15:33 +0500

    feat: Скорректировал разметку и добавил обработку ввода строки поиска

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="search">
-    <form action="#" class="search__form">
-      <input type="text" class="search__input">
-      <button type="submit" class="search__submit">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <form action="#" class="search__form" @submit.prevent="search">
+      <input type="text" class="search__input" :class="text ? 'search__input--filled' : ''" v-model.trim="text" @input="search">
+      <div class="search__label-box">
+        <label class="search__label">Поиск катлогу</label>
+      </div>
+      <button type="submit" class="search__button">
+        <svg class="search__button-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <circle cx="13.5" cy="10.5" r="5.5" stroke="#0066B3" stroke-width="2"/>
           <line x1="9.70711" y1="15.7071" x2="4.70711" y2="20.7071" stroke="#0066B3" stroke-width="2"/>
         </svg>
@@ -15,5 +18,15 @@
 <script>
 export default {
   name: "Search", // eslint-disable-line
+  data() {
+    return {
+      text: '',
+    };
+  },
+  methods: {
+    search() {
+      this.$emit('search', this.text);
+    }
+  },
 }
 </script>
