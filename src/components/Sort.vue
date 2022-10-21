0eaac95bca32f3b9d1ad14194837da7b6bb0a2b4
gruv19 <grv1992@yandex.ru>
2022-10-21 21:18:05 +0500

    feat: Реализовал функционал компонента sort

--- src/components/Sort.vue
+++ src/components/Sort.vue
@@ -1,14 +1,121 @@
 <template>
-  <a href="#" class="search">
-    <span class="search__text">Цене</span>
-    <svg class="search__icon" width="7" height="8" viewBox="0 0 7 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M3.5 0V7M3.5 7L1 4.5M3.5 7L6 4.5" stroke="#EB3737"/>
-    </svg>
-  </a>
+  <div class="sort">
+    <p class="sort__label">Сортировать по:</p>
+    <a 
+      href="#" 
+      class="sort__link" 
+      v-for="property in properties" 
+      :key="property" 
+      @click.prevent="sort(property)" 
+      :class="property.isActive ? 'sort__link--active' : ''"
+    >
+      <span class="sort__text">{{ property.title }}</span>
+      <svg 
+        v-if="property.isActive" 
+        class="sort__icon" 
+        :class="property.inAscending ? '' : 'sort__icon--up'" 
+        viewBox="0 0 7 8" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M3.5 0V7M3.5 7L1 4.5M3.5 7L6 4.5" stroke="#EB3737"/>
+      </svg>
+    </a>
+  </div>
 </template>
 
 <script>
+
+function sortAlphabetAscend(array) {
+  return array.sort((a, b) => {
+    const titleA = a.title;
+    const titleB = b.title;
+    if (titleA < titleB) return -1
+    if (titleA > titleB) return 1
+    return 0;
+  });
+}
+
+function sortAlphabetDescend(array) {
+  return array.sort((a, b) => {
+    const titleA = a.title;
+    const titleB = b.title;
+    if (titleA > titleB) return -1
+    if (titleA < titleB) return 1
+    return 0;
+  });
+}
+
+function sortCostAscend(array) {
+  return array.sort((a, b) => {
+    const costA = a.cost;
+    const costB = b.cost;
+    if (costA < costB) return -1
+    if (costA > costB) return 1
+    return 0;
+  });
+}
+
+function sortCostDescend(array) {
+  return array.sort((a, b) => {
+    const costA = a.cost;
+    const costB = b.cost;
+    if (costA > costB) return -1
+    if (costA < costB) return 1
+    return 0;
+  });
+}
+
 export default {
   name: "Sort", // eslint-disable-line
+  data() {
+    return {
+      properties: [
+        {
+          title: 'Алфавиту',
+          inAscending: true,
+          ascending: sortAlphabetAscend,
+          descending: sortAlphabetDescend,
+          isActive: true,
+        },
+        {
+          title: 'Цене',
+          inAscending: false,
+          ascending: sortCostAscend,
+          descending: sortCostDescend,
+          isActive: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    sort(property) {
+      this.unactivate();
+      property.isActive = true;
+      if (property.inAscending) {
+        this.clearSortOrder();
+        property.inAscending = false;
+        this.$emit('sort', property.descending);
+      } else {
+        this.clearSortOrder();
+        property.inAscending = true;
+        this.$emit('sort', property.ascending);
+      }
+    },
+    unactivate() {
+      this.properties = this.properties.map((property) => {
+        property.isActive = false;
+        return property;
+      });
+    },
+    clearSortOrder() {
+      this.properties = this.properties.map((property) => {
+        property.inAscending = false;
+        return property;
+      });
+    },
+  },
+  mounted() {
+    this.$emit('sort', this.properties[0].ascending);
+  }
 }
 </script>
