e7d470a62f0f7b4c17ca9d2ad8f79e9469b2ec38
gruv19 <grv1992@yandex.ru>
2022-10-22 13:56:47 +0500

    feat: Реализовал функционал компонента Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,22 +1,68 @@
 <template>
-  <div class="card">
-    <img src="https://imgholder.ru/336x300/412234/b49fcc.jpg&text=IMAGE&font=arial" alt="Картинка курса" class="card__img">
-    <a href="#" class="card__overlay">
-      <div class="card__series">Бизнес навыки</div>
-      <h5 class="card__title">Коммерческие переговоры</h5>
-      <div class="card__cost">21 000 ₽</div>
+  <a href="#" class="card">
+    <picture>
+      <source
+        :srcset="desktopImg"
+        media="(min-width: 1366px)"
+      />
+      <source
+        :srcset="tabletImg"
+        media="(min-width: 768px)"
+      />
+      <img
+        class="card__img"
+        :srcset="img"
+      />
+    </picture>
+    <div class="card__overlay">
+      <div class="card__series">{{ series }}</div>
+      <h5 class="card__title">{{ title }}</h5>
+      <div class="card__cost">{{ cost }} {{ currency }}</div>
       <div class="card__detail">
         Подробнее
-        <svg class="card__detail-icon" width="20" height="10" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg class="card__detail-icon" viewBox="0 0 20 10" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M1 5H19M19 5L15.1 9M19 5L15.1 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
       </div>
-    </a>
-  </div>
+    </div>
+  </a>
 </template>
 
 <script>
 export default {
   name: "Card", // eslint-disable-line
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    cost: {
+      type: Number,
+      required: true,
+    },
+    currency: {
+      type: String,
+    },
+    series: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    tabletImg() {
+      return this.img.replace(/(\.jpg)/g, '-tablet'+'$1')
+    },
+    desktopImg() {
+      return this.img.replace(/(\.jpg)/g, '-desktop'+'$1')
+    }
+  },
 }
 </script>
